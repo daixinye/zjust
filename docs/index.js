a1ce42c3b16aec6c12e0151af643242ca78d770a
Dxy <denight@qq.com>
2018-08-31 15:23:43 +0800

    update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,64 +1,111 @@
 void (function () {
-  var shitis = null
-
   window.onload = function () {
-    var xhr = new window.XMLHttpRequest()
-    xhr.open('GET', './data.json', true)
-    xhr.send()
+    var app = new App()
+    app.start()
+  }
 
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        shitis = JSON.parse(xhr.response)
+  function App () {
+    // 初始化变量
+    var _nodes = new Nodes()
+    var _views = new Views()
+    this.shitis = null
+
+    // 初始化方法
+    this.onSearch = function () {
+      var $keyword = _nodes.$keyword
+      var keyword = $keyword.value
+      $keyword.value = ''
+      $keyword.focus()
+
+      if (!keyword) {
+        return
       }
+
+      var questions = this.shitis.questions || []
+      var answers = this.shitis.answers || []
+      var qas = []
+
+      questions.forEach(function (question, index) {
+        if (question.match(new RegExp(keyword, 'g'))) {
+          question = question.replace(
+            new RegExp(keyword, 'g'),
+            ['<mark>', keyword, '</mark>'].join('')
+          )
+          qas.push({
+            question: question,
+            answer: answers[index]
+          })
+        }
+      })
+
+      _views.result.clear()
+      _views.result.print(qas)
+    }
+
+    this.fetchData = function () {
+      var xhr = new window.XMLHttpRequest()
+      xhr.open('GET', './data.json', true)
+      xhr.send()
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          this.onLoaded(xhr.response)
+          this.shitis = JSON.parse(res)
+        }
+      }.bind(this)
+    }
+
+    this.bindEvent = function () {
+      _nodes.$button.addEventListener('click', this.onSearch)
+    }
+
+    // 启动App
+    this.start = function () {
+      // 获取数据
+      this.fetchData()
+      // 绑定事件
+      this.bindEvent()
     }
   }
 
-  window.handleSearch = function () {
-    var $keyword = document.querySelector('#keyword')
-    var keyword = $keyword.value
-    $keyword.value = ''
-    $keyword.focus()
+  function Nodes () {
+    this.$result = document.querySelector('#result')
+    this.$button = document.querySelector('#button')
+    this.$keyword = document.querySelector('#keyword')
+  }
 
-    var questions = shitis.questions || []
-    var answers = shitis.answers || []
-    var qas = []
+  function Views () {
+    var _nodes = new Nodes()
 
-    questions.forEach(function (question, index) {
-      if (question.match(new RegExp(keyword, 'g'))) {
-        question = question.replace(
-          new RegExp(keyword, 'g'),
-          ['<mark>', keyword, '</mark>'].join('')
-        )
-        qas.push({
-          question: question,
-          answer: answers[index]
+    this.button = {
+      disable: function () {
+        _nodes.$button.setAttribute('disabled', 'disabled')
+      },
+      enable: function () {
+        _nodes.$button.setAttribute('disabled', '')
+      }
+    }
+    this.result = {
+      print: function (qas) {
+        var $result = document.querySelector('#result')
+        if (!$result) {
+          throw Error('#result not found')
+        }
+
+        qas.forEach(function (item) {
+          var li = document.createElement('li')
+          li.innerHTML = item.question + item.answer
+          $result.appendChild(li)
         })
+      },
+      clear: function () {
+        var $result = document.querySelector('#result')
+        if (!$result) {
+          throw Error('#result not found')
+        }
+
+        $result.innerHTML = ''
       }
-    })
-
-    window.clearQA()
-    window.printQA(qas)
-  }
-
-  window.printQA = function (qas) {
-    var $result = document.querySelector('#result')
-    if (!$result) {
-      throw Error('#result not found')
     }
-
-    qas.forEach(function (item) {
-      var li = document.createElement('li')
-      li.innerHTML = item.question + item.answer
-      $result.appendChild(li)
-    })
-  }
-
-  window.clearQA = function () {
-    var $result = document.querySelector('#result')
-    if (!$result) {
-      throw Error('#result not found')
-    }
-
-    $result.innerHTML = ''
   }
 })()
